397756fd0896c309aede223b2d0c931deaa7e9af
btolarz <btolarz@eepic.pl>
2014-04-14 11:48:45 +0200

    Fix for many spree payments

--- app/controllers/spree/gateway/przelewy24_controller.rb
+++ app/controllers/spree/gateway/przelewy24_controller.rb
@@ -2,7 +2,7 @@ require 'digest/md5'
 module Spree
   class Gateway::Przelewy24Controller < Spree::BaseController
     skip_before_filter :verify_authenticity_token, :only => [:comeback, :complete]
-    
+
     # Show form Przelewy24 for pay
     def show
       @order = Order.find(params[:order_id])
@@ -10,10 +10,10 @@ module Spree
         @gateway = @order.available_payment_methods.find{|x| x.id == params[:gateway_id].to_i }
         @order.payments.destroy_all
         payment = @order.payments.create!(:amount => 0, :payment_method_id => @gateway.id)
-        
+
         @p24_session_id = Time.now.to_f.to_s
         @p24_crc = przelewy24_transaction_crc(@gateway,@order,@p24_session_id)
-    
+
         if @order.blank? || @gateway.blank?
           flash[:error] = I18n.t("invalid_arguments")
           redirect_to :back
@@ -22,12 +22,12 @@ module Spree
         end
       end
     end
-    
+
     def error
       @order = Order.find(params[:order_id])
     end
-    
-  
+
+
     # Result from Przelewy24
     def comeback
       @order = Order.find(params[:order_id])
@@ -43,11 +43,11 @@ module Spree
         redirect_to gateway_przelewy24_error_path(:gateway_id => @gateway.id, :order_id => @order.id, :error_code => result[2], :error_descr => result[3])
       end
     end
-    
+
     # complete the order
-    def complete    
+    def complete
       @order = Order.find(params[:order_id])
-      
+
       session[:order_id]=nil
       if @order.state=="complete"
         redirect_to order_url(@order, {:checkout_complete => true, :order_token => @order.token}), :notice => I18n.t("payment_success")
@@ -55,28 +55,28 @@ module Spree
         redirect_to order_url(@order)
       end
     end
-    
-  
-  
+
+
+
     private
-  
+
     # validating dotpay message
-    def przelewy24_transaction_crc(gateway,order,session_id)    
+    def przelewy24_transaction_crc(gateway,order,session_id)
       calc_md5 = Digest::MD5.hexdigest(session_id + "|" +
         (@gateway.preferred_p24_id_sprzedawcy.nil? ? "" : @gateway.preferred_p24_id_sprzedawcy) + "|" +
         (@gateway.p24_amount(@order.total).nil? ? "" : @gateway.p24_amount(@order.total)) + "|" +
         (@gateway.preferred_crc_key.nil? ? "" : @gateway.preferred_crc_key))
-        
+
         return calc_md5
-      
+
     end
-    
+
     def przelewy24_verify(gateway,order,params)
       require 'net/https'
       require 'net/http'
       require 'open-uri'
       require 'openssl'
-      
+
       params_new = {:p24_session_id => params[:p24_session_id], :p24_order_id => params[:p24_order_id], :p24_id_sprzedawcy => gateway.preferred_p24_id_sprzedawcy, :p24_kwota => params[:p24_kwota]}
       params_new[:p24_crc] = Digest::MD5.hexdigest(params[:p24_session_id]+"|"+params[:p24_order_id]+"|"+params[:p24_kwota]+"|"+gateway.preferred_crc_key)
       #params_list = "p24_session_id=#{params_new[:p24_session_id]}&p24_order_id=#{params_new[:p24_session_id]}&p24_id_sprzedawcy=#{params_new[:p24_id_sprzedawcy]}&p24_kwota=#{params_new[:p24_kwota]}&p24_crc=#{params_new[:p24_crc]}"
@@ -90,32 +90,30 @@ module Spree
       response = con.start {|http| http.request(req)}
       return response.body
     end
-  
+
     # Completed payment process
     def przelewy24_payment_success(amount)
-      @order.payment.started_processing
-      if @order.total.to_f == amount.to_f      
-        @order.payment.complete     
-      end    
-      
+      @order.payments.last.started_processing
+      if @order.total.to_f == amount.to_f
+        @order.payments.last.complete
+      end
+
       @order.finalize!
-      
+
       @order.next
       @order.next
       @order.save
     end
-  
+
     # payment cancelled by user (dotpay signals 3 to 5)
     def przelewy24_payment_cancel(params)
       @order.cancel
     end
-  
+
     def przelewy24_payment_new(params)
       @order.payment.started_processing
       @order.finalize!
     end
-  
+
   end
 end
-
-
