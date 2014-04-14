802f9452bac2a3e2c2981209317362281af5f872
btolarz <btolarz@eepic.pl>
2014-04-14 11:37:39 +0200

    Fix for rails 4

--- app/models/spree/payment_method/przelewy24.rb
+++ app/models/spree/payment_method/przelewy24.rb
@@ -1,9 +1,9 @@
 module Spree
   class PaymentMethod::Przelewy24 < PaymentMethod
-    
-    
-    attr_accessible :preferred_test_mode, :preferred_url, :preferred_url_transakcja, :preferred_test_url, :preferred_crc_key, :preferred_p24_language, :preferred_test_url_transakcja, :preferred_p24_return_url_error, :preferred_p24_return_url_ok, :preferred_p24_id_sprzedawcy
-  
+
+
+    #attr_accessible :preferred_test_mode, :preferred_url, :preferred_url_transakcja, :preferred_test_url, :preferred_crc_key, :preferred_p24_language, :preferred_test_url_transakcja, :preferred_p24_return_url_error, :preferred_p24_return_url_ok, :preferred_p24_id_sprzedawcy
+
     preference :p24_id_sprzedawcy, :string
     preference :url, :string, :default => "https://secure.przelewy24.pl/index.php"
     preference :url_transakcja, :string, :default => "https://secure.przelewy24.pl/transakcja.php"
@@ -12,15 +12,15 @@ module Spree
     preference :p24_language, :string, :default => "pl"
     preference :crc_key, :string
     preference :test_mode, :boolean, :default => false
-    
+
     def payment_profiles_supported?
       false
     end
-    
+
     def p24_amount(amount)
       (amount*100.00).to_i.to_s #total amount * 100
     end
-    
+
     def post_url
       if preferred_test_mode
         preferred_test_url
@@ -28,7 +28,7 @@ module Spree
         preferred_url
       end
     end
-    
+
     def transakcja_url
       if preferred_test_mode
         preferred_test_url_transakcja
@@ -36,7 +36,6 @@ module Spree
         preferred_url_transakcja
       end
     end
-  
+
   end
 end
-
