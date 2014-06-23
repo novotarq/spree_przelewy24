93db87fb693dc5d9d5e390edcbd9d83f5c813da6
btolarz <btolarz@eepic.pl>
2014-06-23 15:24:47 +0200

    Removed routes for test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-Spree::Core::Engine.routes.append do
-  # Add your extension routes here
-  namespace :gateway do
-    get '/przelewy24/complete/:gateway_id/:order_id' => 'przelewy24#complete', :as => :przelewy24_complete
-    get '/przelewy24/:gateway_id/:order_id' => 'przelewy24#show', :as => :przelewy24
-    get '/przelewy24/error/:gateway_id/:order_id' => 'przelewy24#error', :as => :przelewy24_error
-    post '/przelewy24/comeback/:gateway_id/:order_id' => 'przelewy24#comeback', :as => :przelewy24_comeback
-  end
-end
+# Spree::Core::Engine.routes.append do
+#   # Add your extension routes here
+#   namespace :gateway do
+#     get '/przelewy24/complete/:gateway_id/:order_id' => 'przelewy24#complete', :as => :przelewy24_complete
+#     get '/przelewy24/:gateway_id/:order_id' => 'przelewy24#show', :as => :przelewy24
+#     get '/przelewy24/error/:gateway_id/:order_id' => 'przelewy24#error', :as => :przelewy24_error
+#     post '/przelewy24/comeback/:gateway_id/:order_id' => 'przelewy24#comeback', :as => :przelewy24_comeback
+#   end
+# end
